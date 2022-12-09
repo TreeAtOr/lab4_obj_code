68f1b61109ad8d8e92d2873dc9cea291e7fe0d4f
TreeAtOr <gdorevino@gmail.com>
2022-12-09 18:29:36 +0300

    make beta version

--- lab4_obj_code/AssemblyGenerator.cpp
+++ lab4_obj_code/AssemblyGenerator.cpp
@@ -22,7 +22,7 @@ string make_reference(uniq_id *id, string prefix = string()) {
 }
 
 string const2string(const_base *row, int i, string prefix = string()) {
-  auto declaration = string("const_") + prefix + "_" + to_string(i);
+  auto declaration = string("const_") + prefix + "_" + to_string(i) + ": dq ";
   switch (row->type()) {
   case const_type::_c_int:
     declaration.append(to_string(((const_int *)row)->value));
@@ -42,7 +42,7 @@ string const2string(const_base *row, int i, string prefix = string()) {
 vector<string> make_expression(expression_node *node, string prefix) {
   vector<string> row;
   cout << "make_expression " << node->left_op->op_type()
-       << node->right_op->op_type();
+       << node->right_op->op_type() << endl;
   if (node->left_op->op_type() == operand_type::_id &&
       node->right_op->op_type() == operand_type::_id) {
     auto left_op = dynamic_cast<operand_id *>(node->left_op);
@@ -50,18 +50,18 @@ vector<string> make_expression(expression_node *node, string prefix) {
 
     switch (node->sign_type) {
     case sign_types::_plus:
-      row.push_back(string("mov rax, byte [") +
+      row.push_back(string("mov rax, qword [") +
                     make_reference(right_op->value, prefix) + "]");
-      row.push_back(string("add rax, byte [") +
+      row.push_back(string("add rax, qword [") +
                     make_reference(left_op->value, prefix) + "]");
       row.push_back(string("push rax"));
 
       break;
 
     case sign_types::_minus:
-      row.push_back(string("mov rax, byte [") +
+      row.push_back(string("mov rax, qword [") +
                     make_reference(right_op->value, prefix) + "]");
-      row.push_back(string("sub rax, byte [") +
+      row.push_back(string("sub rax, qword [") +
                     make_reference(left_op->value, prefix) + "]");
       row.push_back(string("push rax"));
       break;
@@ -71,17 +71,14 @@ vector<string> make_expression(expression_node *node, string prefix) {
   if (node->left_op->op_type() == operand_type::_id &&
       node->right_op->op_type() == operand_type::_node) {
     auto left_op = dynamic_cast<operand_id *>(node->left_op);
-    
-    cout << "make_expression complex " << node->left_op->type
-       << node->right_op->type;
-    if (node->right_op->type == node_type::_expression) {
-      auto right_op = dynamic_cast<operand_node *>(node->right_op);
+    auto right_op = dynamic_cast<operand_node *>(node->right_op);
+
+    if (right_op->value->type == node_type::_expression) {
       auto _row = make_expression((expression_node *)right_op->value, prefix);
       row.insert(row.end(), _row.begin(), _row.end());
     }
 
-    if (node->right_op->type == node_types_t::_function_call) {
-      auto right_op = dynamic_cast<operand_node *>(node->right_op);
+    if (right_op->value->type == node_types_t::_function_call) {
       auto _row =
           make_function_call((function_call_node *)right_op->value, prefix);
       row.insert(row.end(), _row.begin(), _row.end());
@@ -90,7 +87,7 @@ vector<string> make_expression(expression_node *node, string prefix) {
     switch (node->sign_type) {
     case sign_types::_plus:
       row.push_back(string("pop rax"));
-      row.push_back(string("add rax, byte [") +
+      row.push_back(string("add rax, qword [") +
                     make_reference(left_op->value, prefix) + "]");
       row.push_back(string("push rax"));
 
@@ -98,7 +95,7 @@ vector<string> make_expression(expression_node *node, string prefix) {
 
     case sign_types::_minus:
       row.push_back(string("pop rax"));
-      row.push_back(string("sub rax, byte [") +
+      row.push_back(string("sub rax, qword [") +
                     make_reference(left_op->value, prefix) + "]");
       row.push_back(string("push rax"));
       break;
@@ -116,24 +113,30 @@ vector<string> make_assignment(assignment_node *node, string prefix) {
     auto left_op = dynamic_cast<operand_id *>(node->left_op);
     auto right_op = dynamic_cast<operand_id *>(node->right_op);
     right_op->value->getType();
-    row.push_back(string("mov rax, byte [") +
+    row.push_back(string("mov rax, qword [") +
                   make_reference(right_op->value, prefix) + "]");
-    row.push_back(string("mov byte [") +
+    row.push_back(string("mov qword [") +
                   make_reference(left_op->value, prefix) + "],rax");
   }
 
   if (node->left_op->op_type() == operand_type::_id &&
       node->right_op->op_type() == operand_type::_node) {
     auto left_op = dynamic_cast<operand_id *>(node->left_op);
-
-    if (node->right_op->type == node_type::_expression) {
-      auto right_op = dynamic_cast<operand_node *>(node->right_op);
+    auto right_op = dynamic_cast<operand_node *>(node->right_op);
+    cout << "make_expression complex " << right_op->value->type << endl;
+    if (right_op->value->type == node_type::_expression) {
       auto _row = make_expression((expression_node *)right_op->value, prefix);
       row.insert(row.end(), _row.begin(), _row.end());
     }
 
+    if (right_op->value->type == node_types_t::_function_call) {
+      auto _row =
+          make_function_call((function_call_node *)right_op->value, prefix);
+      row.insert(row.end(), _row.begin(), _row.end());
+    }
+
     row.push_back(string("pop rax"));
-    row.push_back(string("mov byte [") +
+    row.push_back(string("mov qword [") +
                   make_reference(left_op->value, prefix) + "], rax");
   }
 
@@ -147,9 +150,9 @@ vector<string> make_condition(condition_node *node, string prefix) {
     auto left_op = dynamic_cast<operand_id *>(node->left_op);
     auto right_op = dynamic_cast<operand_id *>(node->right_op);
     right_op->value->getType();
-    row.push_back(string("mov rax, byte [") +
+    row.push_back(string("mov rax, qword [") +
                   make_reference(right_op->value, prefix) + "]");
-    row.push_back(string("cmp byte [") +
+    row.push_back(string("cmp qword [") +
                   make_reference(left_op->value, prefix) + "],rax");
   }
 
@@ -164,7 +167,7 @@ vector<string> make_condition(condition_node *node, string prefix) {
     }
 
     row.push_back(string("pop rax"));
-    row.push_back(string("cmp byte [") +
+    row.push_back(string("cmp qword [") +
                   make_reference(left_op->value, prefix) + "], rax");
   }
 
@@ -288,17 +291,18 @@ vector<string> AssemblyGenerator::build(myTree &root, bool silent) {
 
   rodata.push_back(string("SECTION .rodata"));
   for (int i = 0; i < root.conTable.table.size(); i++) {
-    rodata.push_back(const2string(root.conTable.table.at(i), i) + ": db ");
+    rodata.push_back(const2string(root.conTable.table.at(i), i));
   }
 
   data.push_back(string("SECTION .data"));
   for (int i = 0; i < root.idTable.table.size(); i++) {
     identifier *row = root.idTable.table.at(i);
-    auto declaration = var2string(i) + ": db 0";
+    auto declaration = var2string(i) + ": dq 0";
     data.push_back(declaration);
   }
 
   text.push_back(string("SECTION .text"));
+  text.push_back(string("global main"));
 
   for (auto function : root.functions) {
     auto context = function->name;
@@ -306,16 +310,15 @@ vector<string> AssemblyGenerator::build(myTree &root, bool silent) {
 
     for (int i = 0; i < function->body->conTable.table.size(); i++) {
       rodata.push_back(
-          const2string(function->body->conTable.table.at(i), i, context) +
-          ": db ");
+          const2string(function->body->conTable.table.at(i), i, context) );
     }
 
     for (int i = 0; i < function->body->idTable.table.size(); i++) {
       identifier *row = function->body->idTable.table.at(i);
-      auto declaration = var2string(i, context) + ": db 0";
+      auto declaration = var2string(i, context) + ": dq 0";
       data.push_back(declaration);
     }
-    text.push_back("pop " + var2string(0, context));
+    text.push_back("pop qword [" + var2string(0, context) + "]");
     for (auto command : function->body->root->sons) {
       auto row = make_subprogram(command, context);
       text.insert(text.end(), row.begin(), row.end());
